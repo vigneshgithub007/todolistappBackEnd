9e4b4285dad94bb20e9429bca8b549a5c56549e0
Jarvis <vigneshgithub007@gmail.com>
2019-08-20 22:53:40 +0530

    newfile

---
+++ config/appConfig.js
@@ -0,0 +1,26 @@
+let appConfig = {};
+let nodeMailer = {};
+
+appConfig.port = 3000;
+appConfig.allowedCorsOrigin = "*";
+appConfig.env = "dev";
+appConfig.db = {
+    
+     uri: 'mongodb://127.0.0.1:27017/todoapplist'
+
+}
+  
+appConfig.apiVersion = '/api/v1';
+
+nodeMailer.email="vikkiraj1995@gmail.com";
+nodeMailer.password = "Ravids@123456";
+
+module.exports = {
+    port: appConfig.port,
+    allowedCorsOrigin: appConfig.allowedCorsOrigin,
+    environment: appConfig.env,
+    db :appConfig.db,
+    apiVersion : appConfig.apiVersion,
+    email: nodeMailer.email,
+    password: nodeMailer.password
+};
